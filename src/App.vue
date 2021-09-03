d99d9354307e9b471171758af7b00ca3b61ead52
marcoche88 <marco.che@hotmail.it>
2021-09-03 15:38:47 +0200

    Hero component

--- src/App.vue
+++ src/App.vue
@@ -2,26 +2,35 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
     <Title />
+    <Hero />
   </div>
 </template>
 
 <script>
 import Title from "@/components/Title.vue";
+import Hero from "@/components/Hero.vue";
 export default {
   name: "App",
   components: {
     Title,
+    Hero,
   },
 };
 </script>
 
 <style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+img {
+  width: 100px;
+  margin: 40px 0;
 }
 </style>
---
+++ src/components/Hero.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="jumbotron"></div>
+</template>
+
+<script>
+export default {
+  name: "Hero",
+};
+</script>
+
+<style scoped>
+.jumbotron {
+  height: 500px;
+  background-image: url("../assets/jumbo.jpg");
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  margin-top: 30px;
+}
+</style>
